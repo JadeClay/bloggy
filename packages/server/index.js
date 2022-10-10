a6a0fd1148eed0ca5bdcee96db39702d900c0455
Oscar Pina Pena <isjadeclo@gmail.com>
2022-10-10 17:58:05 -0400

    Major reestructuring on RESTful API

--- packages/server/index.js
+++ packages/server/index.js
@@ -1,34 +1,26 @@
 const dotenv = require('dotenv').config({ path: '../../.env' });
 const mongoose = require('mongoose');
 const cors = require('cors');
-
 const express = require('express');
+const router = require('./routes');
+
 const app = express();
-
-const Post = require('./Models/Post');
-
-/* Connecting database */
-async function testConn(){
-    try {
-        await mongoose.connect(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`).then(res => {if(res){console.log("Database connected succesfully")}});
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-testConn();
 
 app.use(cors());
 
-app.get('/post/last', async (req, res) => {
-    const all = await Post.find({}).limit(1).sort({ _id: -1}).lean();
-    res.status(200).send(all);
-})
+/* Connecting database */
+mongoose.connect(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`)
+    .then(res => {
+        if(res){
+            console.log("Database connected succesfully")
+        }
 
-app.get('/hello', (req, res) => {
-    res.status(200).send({body: "Hello World"});
-})
+        app.use(router);
+        app.listen(process.env.port, () => {
+            console.log(`App listening on port ${process.env.port}`);
+        });
+    });
 
-app.listen(process.env.port);
 
-console.log(`App listening on port ${process.env.port}`);
+
+
---
+++ packages/server/routes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+
+/*Importing all mongodb models */
+const Post = require('./Models/Post');
+
+router.get('/post/last', async (req, res) => {
+    const all = await Post.find({}).limit(1).sort({ _id: -1}).lean();
+    res.status(200).send(all);
+})
+
+router.get('/hello', (req, res) => {
+    res.status(200).send({body: "Hello World"});
+})
+
+module.exports = router
