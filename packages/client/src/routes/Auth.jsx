4e575673de0b29d9200e6f7605cacccce0bbc8d9
Oscar Pina Pena <isjadeclo@gmail.com>
2022-10-16 21:24:11 -0400

    Added functional login and register endpoints

---
+++ packages/client/src/routes/Auth.jsx
@@ -0,0 +1,15 @@
+import { Stack } from '@mui/system';
+import React from 'react';
+import Navbar from '../Components/Navbar/Navbar';
+
+export default function Auth() {
+  return (
+    <>
+        <Navbar/>
+
+        <Stack>
+            
+        </Stack>
+    </>
+  )
+}
